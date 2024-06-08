ce445873d7a5144db57fa2e3fa91d73a92b12791
Simar-4202 <simarpreetsingh4202@gmail.com>
2024-06-08 10:52:55 +0000

    container config done

---
+++ 01_basics/problems.js
@@ -0,0 +1,33 @@
+// let v=18;
+// let x=18;
+// let w=18;
+// let y=18;
+// console.log(v==w);
+// console.log(v==w==x);
+// console.log(v==w==x==y);
+// console.log(v==w==x==y==0);
+
+// let str="Hey Js! you are amazing i think";
+// let vowels = ["a", "e", "i", "o", "u"];
+
+// str = str.toLowerCase();
+// console.log(str);
+// let count=0;
+
+// myArr=Array.from(str);
+// console.log(myArr);
+// myArr.includes(vowels);
+
+let vari="hello doston kaise ho sabhi umid krta hu ache honge"
+let vowels=["a","e","i","o","u"]
+let count=0;
+let arr=Array.from (vari);
+console.log(arr.length);
+arr.forEach(element => {
+   if(element="a"){
+    count++;
+   }
+   
+    
+});
+console.log(count);
