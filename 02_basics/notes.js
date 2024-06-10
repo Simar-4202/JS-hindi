9d72774f8ce9f895d5382b4820c8b619c1d23ee2
Simar-4202 <simarpreetsingh4202@gmail.com>
2024-06-10 12:37:10 +0000

    container config done

---
+++ 02_basics/notes.js
@@ -0,0 +1,31 @@
+/*
+array are made using square brackets []
+to access
+arr[1];
+
+push()-> to ADD elements at the end of the array
+pop()-> to REMOVE elements at the end of the array
+
+
+unshift()-> to ADD elements at the start of the array
+shift()-> to REMOVE elements at the start of the array
+-->used less as it change all the indexes of other elements
+
+
+questions
+includes()--> will give true if elemnt is present
+indexof() --> will give -1 if not present and index no. when present
+join() --> change type of array to string
+slice(-4,-7) -->will cut some part of array and will not effect original one
+splice(4,7) -->will cut some part of array and will manipulate original one
+Array.isArray() --> asking whether its an array or not
+*/
+
+
+//use spread operator to add 2 or more arrays
+// [...arr1 ,...arr2];
+
+
+// arr.flat(infinity)
+// Array.from("simar")--> to make array from simar
+// Array.of(score1,score2,score3)
---
+++ 02_basics/notesObj.js
@@ -0,0 +1,32 @@
+/*
+// Object has the pair of key:value
+let JsUser={}
+// can assign values by 
+JsUser.email="simar@com"
+console.log(JsUser);
+// can be changed by  
+JsUser.email="chatGPT@com"
+// can be accessed by  
+console.log(JsUser.email) //or 
+console.log(JsUser["email"])
+
+[symbol] ="mykey"  //to write key of datatype symbol
+can be accessed by JsUser[symbol]
+*/
+
+//Object.freeze(JsUser)
+//Object.keys(JsUser)
+//Object.values(JsUser)
+//Object.entries(JsUser)
+
+//{...obj1,...obj2} to join 2 or more objects
+
+//.hasOwnProperty helps to find the keys present 
+// if true key is present 
+// if false key is absent
+
+
+
+//destructuring 
+
+//const {courseInstructor}=course
